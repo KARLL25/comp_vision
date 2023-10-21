69fc8aed932a8f5cacea664a3a15849a78dabd87
daiki <51706588+KARLL25@users.noreply.github.com>
2023-10-21 09:58:13 +0800

    task from lesson

---
+++ 2zadanie/wires.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.morphology import (binary_erosion, binary_dilation, binary_opening, binary_closing)
+
+from skimage.measure import label
+
+
+wires =np.load(r"wires6.npy")
+struct = np.ones((3,2))
+
+labeled = label(wires)
+for lb in range(1, np.max(labeled)+1):
+    new_image = np.zeros_like(wires)
+    new_image[labeled==lb] = 1
+    print("провода", lb)
+    eroded_wire = binary_erosion(new_image, struct)
+    parts = np.max(label(eroded_wire))
+
+    if parts == 1:
+        print(f"Провод {lb} не порван.")
+    else:
+        print(f"Провод {lb} порван на {parts} части.")
+
+
+
+
+plt.subplot(121)
+plt.imshow(labeled)
+plt.show()
+
